7ad6251207ca2b2f2d484003ddb7fcc3165828d2
saasbook <saasbook@saasbook.(none)>
2013-10-29 21:14:58 -0700

    removed events dependency to fix initial run error.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ProjectSteak::Application.config.secret_token = ENV['SECRET_TOKEN']
+ProjectSteak::Application.config.secret_token = '4013bb6b7d42dc258bd195650471da7cc5a401cd39d698bf665f7588186e58b6a02f25e8143ad60629e55bf6d82212cb2757adfa8f41cfe311f9c1283baa9b60
+'
--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :person do
     name 'A Fake Name'
     progress '1'
-    events [Event.create!(:name => 'Fake Event 1')]
+    #events [Event.create!(:name => 'Fake Event 1')]
   end
   #factory :event do
    # name 'A Fake Event'
