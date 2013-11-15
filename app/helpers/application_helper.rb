89525ad084bea2cd25ada8812b5d0c9e11d914d1
saasbook <saasbook@saasbook.(none)>
2013-11-14 21:39:45 -0800

    People list implemented. Ordering possible by name or progress. Title bar clickable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,21 @@ require 'open-uri'
 
 module ApplicationHelper
 
-    class NetworkError < RuntimeError ; end
+  class NetworkError < RuntimeError ; end
 
-    def getXML(uri)
-        begin
-          doc = Nokogiri::XML(open(uri))
-        rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-            Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
-            Net::ProtocolError => e 
-            raise ProjectSteak::NetworkError, e
-        end
+  def getXML(uri)
+    begin
+      doc = Nokogiri::XML(open(uri))
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
+        Net::ProtocolError => e 
+        raise ProjectSteak::NetworkError, e
     end
+  end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    order = (column == params[:sort] && params[:order] == 'asc') ? 'desc' : 'asc'
+    link_to title, :sort =>column, :order => order
+  end
 end
