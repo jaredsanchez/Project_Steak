d9376961f28d84b69c6b79f99027a2ccae280214
saasbook <saasbook@saasbook.(none)>
2013-11-17 01:57:37 -0800

    Cuke tests added

--- features/step_definitions/person_steps.rb
+++ features/step_definitions/person_steps.rb
@@ -2,6 +2,26 @@ Given /the following people exist/ do |people_table|
   people_table.hashes.each do |person|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that person to the database here.
-    Person.create!(:name => person[:name], :progress => person[:progress])
+    Person.create!(:first_name => person[:first_name], :last_name => person[:last_name], :progress => person[:progress], :favorite => person[:favorite])
   end
 end
+
+
+Then /I should see '(.*)' before '(.*)'/ do |e1, e2|
+	body = page.find('#body').text
+	body.index(e1).should < body.index(e2)
+end
+
+
+Then /^I should see the following people in this order: (.*)/ do |people_list|
+	sorted_people = []
+	people_list.split(', ').each do |person|
+		sorted_people.push(person)
+	end
+	sorted_people.each_cons(2) do |a, b|
+		step %{I should see #{a} before #{b}}
+	end
+end
+
+
+
