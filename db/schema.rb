6058be528041f681340c333655dd752280e46659
jaredsanchez <jared.sanchez@gmail.com>
2013-11-23 03:54:20 -0800

    working except for oauth access token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131121011128) do
+ActiveRecord::Schema.define(:version => 20131123110658) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -34,9 +34,10 @@ ActiveRecord::Schema.define(:version => 20131121011128) do
     t.string   "name"
     t.integer  "progress"
     t.integer  "events_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
     t.string   "email"
+    t.boolean  "is_linkedin_connection"
   end
 
   create_table "users", :force => true do |t|
