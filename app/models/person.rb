136bb1a89acc3c9d0698620b73e53861620a0ab0
John Gunnison <johngun3@gmail.com>
2013-11-23 16:30:26 -0800

    modified person.rb to handle first and last name

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,20 +4,28 @@ class Person < ActiveRecord::Base
     extend ApplicationHelper
 	
     has_and_belongs_to_many :events
-	attr_accessible :name, :progress, :active
+	attr_accessible :name, :first_name, :last_name, :progress, :active
 
     def self.processPeople(org_unit)
         uri = "https://apis.berkeley.edu/calnet/person?searchFilter" +
-          "=(departmentNumber=" + org_unit['org_node'] + ")&attributesToReturn=berkeleyEduFirstName,berkeleyEduLastName" +
+          "=(departmentNumber=" + org_unit['org_node'] + ")&attributesToReturn=displayName,berkeleyEduFirstName,berkeleyEduLastName" +
           #"=(departmentNumber=" + "EH1EO" + ")&attributesToReturn=displayname" +  
           "&app_id=" + ENV['CAL_NET_ID'] + "&app_key=" + ENV['CAL_NET_KEY']
         doc = getXML(uri)
         doc.xpath('//person').map do |person|
-          first_name = person.xpath('berkeleyedufirstname').inner_text.split(" ").first.capitalize
-          last_name = person.xpath('berkeleyedulastname').inner_text.capitalize
+          first_name = person.xpath('berkeleyedufirstname').inner_text.split(" ")
+          if first_name != []
+            first_name = first_name.first.capitalize
+            last_name = person.xpath('berkeleyedulastname').inner_text.capitalize
+          else
+            name = person.xpath('displayname').inner_text.split(" ")
+            first_name = name.first.capitalize
+            last_name = name.last.capitalize
+          end
           updated_person = Person.find_or_initialize_by_first_name_and_last_name(first_name, last_name)
           new_person = Hash.new
-          new_person['name'] = name
+          new_person['first_name'] = first_name
+            new_person['last_name'] = last_name
           new_person['progress'] = 1
           new_person['active'] = true
           updated_person.update_attributes(new_person)
