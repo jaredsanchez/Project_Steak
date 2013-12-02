4fbf81d7791603c38484b603a88102dbede081d6
saasbook <saasbook@saasbook.(none)>
2013-12-01 17:10:23 -0800

    Typo fix in Cuke tests.

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,11 +1,13 @@
 Then /^I should see the all of the following: (.*)/ do |items_list|
-	people_list.split(', ').each do |item|
+	items_list.split(', ').each do |item|
 		step %{I should see "#{item}"}
 	end
 end
 
 Then /^I should not see the all of the following: (.*)/ do |items_list|
-	people_list.split(', ').each do |item|
+	items_list.split(', ').each do |item|
 		step %{I should not see "#{item}"}
 	end
 end
+
+
